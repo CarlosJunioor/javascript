14215887f3a7063902fa0cb4a761cec37c0195a6
Carlos Junior <104463604+CarlosJunioor@users.noreply.github.com>
2022-05-19 17:09:07 -0300

    js learning

---
+++ aula04/code.js
@@ -0,0 +1,19 @@
+let name
+let age
+let nationality
+let sex
+let student = true
+
+/*
+name = prompt('Whats your name?')
+age = prompt('How old are you')
+nationality = prompt('Whats your nationality')
+sex = prompt('Whats ur gender')
+
+document.getElementById("id1").innerHTML = "Hello,"+ name
+document.getElementById("id2").innerHTML = "Your are ,"+ age + " old."
+document.getElementById("id3").innerHTML = "And you are ,"+ nationality
+document.getElementById("id4").innerHTML = "And your sex is,"+ sex
+document.getElementById("id5").innerHTML = " University accepted was "+ student
+
+*/
