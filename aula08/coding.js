e37ecd8146dea76e3a8100fd9286f32fa66c4e8d
Carlos Junior <104463604+CarlosJunioor@users.noreply.github.com>
2022-05-23 22:20:36 -0300

    update

---
+++ aula08/coding.js
@@ -0,0 +1,8 @@
+let ano = document.getElementById('nasci')
+let sex = document.getElementById('input[name="sex"]:checked').value
+let res = document.getElementById('res')
+
+function verificar(){
+    res.innerHTML= "test 123"
+
+}
