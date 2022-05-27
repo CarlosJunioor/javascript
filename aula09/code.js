fc4ca11d07745b9e79dd64e846915e3daa998bbb
Carlos Junior <104463604+CarlosJunioor@users.noreply.github.com>
2022-05-27 18:00:33 -0300

    Create code.js

---
+++ aula09/code.js
@@ -0,0 +1,10 @@
+let c = 1
+let pizza = 21
+
+while (c < pizza) {
+    console.log(`Essa fatia Nº ${c} está uma delicia`)
+    c+1
+    if (c=pizza){
+        console.log("Terminei de comer a pizza")
+    }
+}
