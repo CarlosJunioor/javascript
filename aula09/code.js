22b64ef2ddd791670fe7bf5a7a70b7de10902f94
Carlos Junior <104463604+CarlosJunioor@users.noreply.github.com>
2022-05-27 18:33:16 -0300

    Update code.js

--- aula09/code.js
+++ aula09/code.js
@@ -1,6 +1,12 @@
 let c = 1
 
+do {
+    console.log(`Essa fatia Nº ${c} está uma delicia`)
+    c++
+} while (c < 11)
+
+/*
 while (c < 10) {
     console.log(`Essa fatia Nº ${c} está uma delicia`)
     c++
-}
+} */
