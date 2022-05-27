282c52f8eead09f520eb07d9a4874f2f1f7e0665
Carlos Junior <104463604+CarlosJunioor@users.noreply.github.com>
2022-05-27 18:13:15 -0300

    Update code.js

--- aula09/code.js
+++ aula09/code.js
@@ -1,10 +1,6 @@
 let c = 1
-let pizza = 21
 
-while (c < pizza) {
+while (c < 10) {
     console.log(`Essa fatia Nº ${c} está uma delicia`)
-    c+1
-    if (c=pizza){
-        console.log("Terminei de comer a pizza")
-    }
+    c++
 }
