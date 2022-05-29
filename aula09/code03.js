09ffa5be27d250e806b58a431a7b2fdf67caf440
Carlos Junior <104463604+CarlosJunioor@users.noreply.github.com>
2022-05-29 10:34:30 -0300

    updt

---
+++ aula09/code03.js
@@ -0,0 +1,7 @@
+let num = [4, 6, 8, 7]
+
+num.push(1)
+num.sort()
+console.log(num)
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`O primeiro vetor é ${num[0]}`)
---
+++ aula09/vetornatela.js
@@ -0,0 +1,9 @@
+let val = [7,4,6,8,3,9,5,2]
+
+/*
+for(let pos = 0 ;pos < val.length; pos++){
+    console.log(`Estamos na posição ${pos} possui o valor ${val[pos]}`)
+}*/
+for (let pos in num){
+    console.log(`A posição ${pos} tem o valor de ${num[pos]}`)
+}
