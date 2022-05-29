64c123e94c44cb94568be00e7aab5d8f199e902d
Carlos Junior <104463604+CarlosJunioor@users.noreply.github.com>
2022-05-29 20:44:22 -0300

    project

---
+++ project-analisadordenum/coding.js
@@ -0,0 +1,9 @@
+let num = document.getElementById("fnum")
+let lista = document.getElementById("flista")
+let res = document.getElementById("res")
+let valores = []
+
+function adicionar(){
+    
+
+}
